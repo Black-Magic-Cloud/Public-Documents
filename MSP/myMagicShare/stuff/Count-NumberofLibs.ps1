4fb2d9615c32ae5296ead7a0bbd6c80fbdca5891
Andreas Hähnel <andreas.haehnel@blackmagiccloud.de>
2023-02-15 17:58:47 +0100

    connection issue fixed

--- MSP/myMagicShare/stuff/Count-NumberofLibs.ps1
+++ MSP/myMagicShare/stuff/Count-NumberofLibs.ps1
@@ -3,7 +3,7 @@ $SPOSiteName = "myMagicShare"
 $connection = Connect-PnPOnline -Url "$($SPOTenantPath)/sites/$($SPOSiteName)" -Interactive -ReturnConnection:$true
 if(!$connection){Write-Host "Error connecting to PnP" -ForegroundColor red;exit}
 
-$lists = Get-PnPList
+$lists = Get-PnPList -Connection $connection
 $LibCount = 0
 foreach ($list in $lists) {
   if (($list.BaseType -eq "GenericList") -or ($list.BaseType -eq "DocumentLibrary") ) {
